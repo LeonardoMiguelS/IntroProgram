34229cb2ed2176a78303b8d3400274396eeca79b
LeonardoMiguelS <144558929+LeonardoMiguelS@users.noreply.github.com>
2023-09-12 15:43:41 -0600

    Add files via upload

---
+++ holamundo.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>  //Se hace la llamada a la Libreria que contiene la funcion printf
+
+int main(){ //Se declara el inicio del programa 
+	printf("Hola Mundo");//palabra reservada printf muestra el mensaje entrecomillado en la consola 
+}
